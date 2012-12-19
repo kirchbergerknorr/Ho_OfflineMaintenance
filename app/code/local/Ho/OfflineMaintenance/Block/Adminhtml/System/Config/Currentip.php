311ff647a0922ab3d4a1b50fdcff33c36b964657
paales <paul@h-o.nl>
2012-12-19 20:31:26 +0100

    Added the current IP. Closes #7

---
+++ app/code/local/Ho/OfflineMaintenance/Block/Adminhtml/System/Config/Currentip.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * H&O Module Ho_OfflineMaintenance
+ * Fork of the ArsOnIt_OfflineMaintenance module: http://www.magentocommerce.com/magento-connect/maintenance-page-artson-it.html
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the H&O Commercial License
+ * that is bundled with this package in the file LICENSE_HO.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://www.h-o.nl/license
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to info@h-o.com so we can send you a copy immediately.
+ *
+ * @category    Ho
+ * @package     Ho_OfflineMaintenance
+ * @copyright   Copyright (c) 2012 H&O (http://www.h-o.nl/)
+ * @license     H&O Commercial License (http://www.h-o.nl/license)
+ */
+/**
+ * @category   Ho
+ * @package    Ho_OfflineMaintenance
+ * @author     H&O Developers <info@h-o.nl>
+ */
+?>
+<?php
+class  Ho_OfflineMaintenance_Block_Adminhtml_System_Config_Currentip extends Mage_Adminhtml_Block_System_Config_Form_Field
+{
+    /**
+     * @param Varien_Data_Form_Element_Abstract $element
+     * @return string
+     */
+    protected function _getElementHtml(Varien_Data_Form_Element_Abstract $element)
+    {
+        /* @var $coreHttp Mage_Core_Helper_Http */
+        $coreHttp = Mage::helper('core/http');
+
+        return  $coreHttp->getRemoteAddr();
+    }
+}
